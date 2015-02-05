a4ead1bcbbf5aa33d2abb09772d83eb68d7c4366
kjlee <ranian129@gmail.com>
2015-02-06 01:00:40 +0900

    modify changer 1.3.

--- spring31study-1.3/src/springbook/user/dao/UserDao.java
+++ spring31study-1.3/src/springbook/user/dao/UserDao.java
@@ -29,18 +29,31 @@ import springbook.user.domain.User;
  *    - 두 개의 클래스가 서로 긴밀하게 연결되어 있지 않도록 중간에 추상적인 느슨한 연결고리를 만듬.
  *    - 문제점:
  *    	1. 여전히 클래스 이름이 UserDao 에 나오고 있다.
+ *  1.3.3 관계설정 책임의 분리.
+ *    - 문제의 원인: UserDao 안에 분리되지 않은, 또 다른 관심사항이 존재하고 있다 -> DB 연결관련.
+ *    - 해결방법: 다른 관심을 분리해서 클라이언트에게 넘긴다.
+ *  1.3.4 원칙과 패턴.
+ *    - 개방 폐쇄 원칙 (OCP)
+ *    	: 클래스나 모듈은 확장에는 열려 있어야 하고 변경에는 닫혀 있어야 한다.
+ *    - 높은 응집도와 낮은 결합도.
+ *    	- 높은 응집도: 변화가 일어날 때 해당 모듈에서 변하는 부분이 크다는 것.
+ *    	- 낮은 결합도: 하나의 변경이 발생할 때 최대한 다른 모듈이나 객체는 수정할 필요 없도록 만드는 것.
+ *    - 전략 패턴 Strategy pattern
+ *    	- 필요에 따라 변경이 필요한 알고리즘(기능)을 인터페이스를 통해 통째로 외부로 분리시키고,
+ *       이를 구현한 구체적인 알고리즘 클래스를 필요에 따라 바꿔서 사용할 수 있게 하는 디자인 패턴.
+ *      - UserDaoTest -> Userdao -> ConnectionMaker <- DConnectionMaker
  * 
  * @author kjlee
  *
  */
 public class UserDao {
 
-	// 인터페이스를 통해 오브젝트에 접근하므로 구체적인 클래스 정보를 알 필요가 없다.
 	private ConnectionMaker connectionMaker;
-	
+
 	// 상태를 관리하는 것도 아니니 한 번만 만들어 인스턴스 변수에 저장해두고 메소드에서 사용하게 한다.
-	public UserDao() {
-		connectionMaker = new DConnectionMaker();	// 앗!! 그런데 여기에는 클래스 이름이 나오네!!!
+	public UserDao(ConnectionMaker connectionMaker) {
+//		connectionMaker = new DConnectionMaker();	// 앗!! 그런데 여기에는 클래스 이름이 나오네!!!
+		this.connectionMaker = connectionMaker; 
 	}
 	
 	public void add(User user) throws ClassNotFoundException, SQLException {
@@ -79,23 +92,4 @@ public class UserDao {
 		return user;
 	}
 	
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		
-		UserDao dao = new UserDao();
-		
-		User user = new User();
-		user.setId("whiteship");
-		user.setName("백기선");
-		user.setPassword("married");
-		
-		dao.add(user);
-		
-		System.out.println(user.getId() + " 등록 성공!");
-		
-		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
-		
-		System.out.println(user2.getId() + " 조회 성공!");
-	}
 }
