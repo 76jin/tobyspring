bfc4b14512bb205d2b98b15a20373f5d90cf5664
kjlee <ranian129@gmail.com>
2015-02-14 02:42:41 +0900

    upgradeLevels() 코드 리팩토링 - Chapter 5.1.5  - upgradeLevels() 리팩토링  - User 테스트 코드 추가  - UserServiceTest 코드 리팩토링

---
+++ spring31study-5.1.5-refactoring/src/springbook/user/service/UserServiceTest.java
@@ -0,0 +1,93 @@
+package springbook.user.service;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/test-applicationContext.xml")
+public class UserServiceTest {
+	
+	public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
+	public static final int MIN_RECOMMEND_FOR_GOLD = 30;
+	
+	@Autowired
+	UserService userService;
+	@Autowired
+	UserDao userDao;
+	
+	List<User> users;
+	
+	@Before
+	public void setUp() {
+		users = Arrays.asList(
+				new User("bumjin", "박범진", "p1", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER-1, 0),
+				new User("joytouch", "강명성", "p2", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER, 0),
+				new User("erwins", "신승한", "p3", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD-1),
+				new User("madnite1", "이상호", "p4", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD),
+				new User("green", "오민규", "p5", Level.GOLD, 100, Integer.MAX_VALUE)
+		);
+	}
+	
+	@Test
+	public void bean() {
+		assertThat(this.userService, is(notNullValue()));
+	}
+	
+	@Test
+	public void upgrades() {
+		userDao.deleteAll();
+		for (User user : users)  userDao.add(user);
+		
+		userService.upgradeLevels();
+		
+//		checkLevel(users.get(0), Level.BASIC);
+		checkLevelUpgraded(users.get(0), false);
+		checkLevelUpgraded(users.get(1), true);
+		checkLevelUpgraded(users.get(2), false);
+		checkLevelUpgraded(users.get(3), true);
+		checkLevelUpgraded(users.get(4), false);
+	}
+
+	private void checkLevelUpgraded(User user, boolean upgraded) {
+		User userUpdate = userDao.get(user.getId());
+		if (upgraded) {
+			assertThat(userUpdate.getLevel(), is(user.getLevel().nextLevel()));
+		} else {
+			assertThat(userUpdate.getLevel(), is(user.getLevel()));
+		}
+	}
+	
+	@Test
+	public void add() {
+		userDao.deleteAll();
+		
+		User userWithLevel = users.get(4);	// GOLD level
+		User userWithoutLevel = users.get(0);
+		userWithoutLevel.setLevel(null);	// No level
+		
+		userService.add(userWithLevel);
+		userService.add(userWithoutLevel);
+		
+		User userWithLevelRead =  userDao.get(userWithLevel.getId());
+		User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+		
+		assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel()));
+		assertThat(userWithoutLevelRead.getLevel(), is(Level.BASIC));
+	}
+
+}
