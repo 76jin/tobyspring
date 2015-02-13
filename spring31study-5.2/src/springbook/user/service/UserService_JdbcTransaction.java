92ad1b0b9c34f8615be5380d634d6dcebec941f9
kjlee <ranian129@gmail.com>
2015-02-14 06:39:33 +0900

    트랜잭션 서비스 추상화 - Chapter 5.2

---
+++ spring31study-5.2/src/springbook/user/service/UserService_JdbcTransaction.java
@@ -0,0 +1,76 @@
+package springbook.user.service;
+
+import java.sql.Connection;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+public class UserService_JdbcTransaction {
+	
+	public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
+	public static final int MIN_RECOMMEND_FOR_GOLD = 30;
+
+	private DataSource dataSource;
+	UserDao userDao;
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+	
+	public void upgradeLevels() throws Exception {
+		TransactionSynchronizationManager.initSynchronization(); // 동기화 초기화
+		Connection c = DataSourceUtils.getConnection(dataSource);
+		c.setAutoCommit(false); 	// 트랜잭션 시작
+				
+		try {
+			List<User> users = userDao.getAll();
+			for (User user : users) {
+				if (canUpgradeLevel(user)) {
+					upgradeLevel(user);
+				}
+			}
+			c.commit();
+		} catch (Exception e) {
+			c.rollback();
+			throw e;
+		} finally {
+			DataSourceUtils.releaseConnection(c, dataSource);
+			TransactionSynchronizationManager.unbindResource(this.dataSource);
+			TransactionSynchronizationManager.clearSynchronization(); // 트랜잭션 종료
+		}
+	}
+
+	protected void upgradeLevel(User user) {
+		user.upgradeLevel();
+		userDao.update(user);
+	}
+
+	private boolean canUpgradeLevel(User user) {
+		Level currentLevel = user.getLevel();
+		switch(currentLevel) {
+			case BASIC: return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
+			case SILVER: return (user.getRecommend() >= MIN_RECOMMEND_FOR_GOLD);
+			case GOLD: return false;
+			default: throw new IllegalArgumentException("Unknown Level:"+currentLevel);
+		}
+	}
+
+	public void add(User user) {
+		if (user.getLevel() == null) user.setLevel(Level.BASIC);
+		userDao.add(user);
+	}
+	
+	
+	
+}
